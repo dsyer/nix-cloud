8cd89ab6cae620158f63746bd5ae56debbc2a3ba
Dave Syer <dsyer@pivotal.io>
2020-04-27 11:17:40 +0100

    Add comment docs

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,10 +5,14 @@ if ! [ -e ~/.config/nixpkgs/config.nix]; then
     ln -s ~/nix-cloud/.config/nixpkgs/config.nix ~/.config/nixpkgs
 fi
 
-echo User: $USER
-
 if ! which nix-env 2> /dev/null; then
+  # Install nix
   curl https://nixos.org/nix/install | bash
+  # Initialize env vars
   . ~/.nix-profile/etc/profile.d/nix.sh
-  nix-env -iA nixpkgs.userPackages
+fi
+
+if ! nix-env -q | grep -q user-packages; then
+  # Install user packages from ~/.config/nixpkgs
+  nix-env -i user-packages
 fi
